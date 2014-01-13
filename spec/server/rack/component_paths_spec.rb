647be218ec994024f70dcd41fc6dd3db7dc90197
Ryan Stout <ryanstout@gmail.com>
2014-01-13 12:08:06 -0700

    WIP

--- spec/server/rack/component_paths_spec.rb
+++ spec/server/rack/component_paths_spec.rb
@@ -2,14 +2,18 @@ require 'volt/server/rack/component_paths'
 
 describe ComponentPaths do
   before do
-    @spec_app_root = File.join(File.dirname(__FILE__), "../../app")
+    spec_app_root = File.join(File.dirname(__FILE__), "../../app")
+    
+    path_to_main = File.join(File.dirname(__FILE__), "../../app/main")
+    @component_paths = ComponentPaths.new(spec_app_root)
+  end
+  
+  it "should return the paths to all app folders" do
+    expect(@component_paths.app_folders).to eq([''])
   end
   
   it "should return the path to a component" do
-    path_to_main = File.join(File.dirname(__FILE__), "../../app/main")
-    component_paths = ComponentPaths.new(@spec_app_root)
-    
-    main_path = component_paths.component_path('main')
+    main_path = @component_paths.component_path('main')
     expect(main_path).to eq('')
   end
 end
