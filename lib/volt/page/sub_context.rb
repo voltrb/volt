d2ca09417b2ed3a537482dff3f54ff95441dbd2c
Ryan Stout <ryanstout@gmail.com>
2014-01-22 12:09:44 -0700

    Spec for sub context respond to

--- lib/volt/page/sub_context.rb
+++ lib/volt/page/sub_context.rb
@@ -10,6 +10,10 @@ class SubContext
     @context = context
   end
   
+  def respond_to?(method_name)
+    !!(@locals[method_name.to_s] || (@context && @context.respond_to?(method_name)) || super)
+  end
+  
   def method_missing(method_name, *args, &block)
     method_name = method_name.to_s
     if @locals[method_name]
---
+++ spec/page/sub_context_spec.rb
@@ -0,0 +1,10 @@
+require 'volt/page/sub_context'
+
+describe SubContext do
+  it "should respond_to correctly on locals" do
+    sub_context = SubContext.new({:name => 'Name'})
+    
+    expect(sub_context.respond_to?(:name)).to eq(true)
+    expect(sub_context.respond_to?(:missing)).to eq(false)
+  end
+end
