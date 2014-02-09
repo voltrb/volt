b23507aa9b830fb8cb71eb2a27c764c782b7a01a
Ryan Stout <ryanstout@gmail.com>
2014-02-09 15:38:44 -0700

    jumbotron default theme

--- templates/project/app/home/config/dependencies.rb
+++ templates/project/app/home/config/dependencies.rb
@@ -1,5 +1,8 @@
 # Specify which components you wish to include when 
 # the "home" component loads.
 
-# the bootstrap theme
+# bootstrap css framework
 component 'bootstrap'
+
+# a default theme for the bootstrap framework
+component 'bootstrap-jumbotron-theme'
