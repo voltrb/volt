5af747fe24bf2abfe3f462bda2d76939f499b6d3
Ryan Stout <ryanstout@gmail.com>
2014-02-11 12:30:14 -0700

    Pass page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,15 @@
 
 require 'volt'
 
+if RUBY_PLATFORM != 'opal'
+  RSpec.configure do |config|
+    config.run_all_when_everything_filtered = true
+    config.filter_run :focus
 
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    config.order = 'random'
+  end
 end
