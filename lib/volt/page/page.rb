cd1195f268db32b6c3a4765f3317d1f940d6715b
DFYX <dfyx@wecallit42.de>
2014-09-19 03:34:50 +0200

    Display message for 2 seconds after reconnecting

--- lib/volt/page/page.rb
+++ lib/volt/page/page.rb
@@ -65,6 +65,14 @@ class Page
 
     # Initialize tasks so we can get the reload message
     self.tasks if Volt.env.development?
+
+    channel.on('reconnected') do
+      @page._reconnected.cur = true
+
+      `setTimeout(function() {`
+        @page._reconnected.cur = false
+      `}, 2000);`
+    end
   end
 
   def flash
