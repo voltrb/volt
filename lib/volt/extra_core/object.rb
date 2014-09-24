9d1040040537b4b755cda5a462203a89e4114d65
Ryan Stout <ryanstout@gmail.com>
2014-09-23 18:44:43 -0600

    WIP

--- lib/volt/extra_core/object.rb
+++ lib/volt/extra_core/object.rb
@@ -6,6 +6,26 @@ class Object
     Hash[instance_variables.map { |name| [name[1..-1], instance_variable_get(name)] }]
   end
 
+  # Provides the same functionality as ||, but since ReactiveValue's only
+  # work with method calls, we provide .or as a convience.
+  def or(other)
+    if self.true?
+      return self
+    else
+      return other
+    end
+  end
+
+  # Provides the same functionality as &&, but since ReactiveValue's only
+  # work with method calls, we provide .and as a convience
+  def and(other)
+    if self.true?
+      return other
+    else
+      return self
+    end
+  end
+
   def html_inspect
     inspect.gsub('<', '&lt;').gsub('>', '&gt;')
   end
