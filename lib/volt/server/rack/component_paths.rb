db154f5151a1023442ddc50b600295f3cc81860b
Ryan Stout <ryanstout@gmail.com>
2014-10-28 16:41:23 -0600

    temp disable

--- lib/volt/server/rack/component_paths.rb
+++ lib/volt/server/rack/component_paths.rb
@@ -76,18 +76,18 @@ module Volt
           end
         end
 
-        component_names = []
-        app_folders do |app_folder|
-          Dir["#{app_folder}/*"].map {|cp| cp[/[^\/]+$/] }.each do |component_name|
-            component_names << component_name
-          end
-        end
-
-        # Load in all views
-        component_names.uniq.each do |component_name|
-          code = Volt::ComponentCode.new(component_name, self).code
-          eval(code)
-        end
+        # component_names = []
+        # app_folders do |app_folder|
+        #   Dir["#{app_folder}/*"].map {|cp| cp[/[^\/]+$/] }.each do |component_name|
+        #     component_names << component_name
+        #   end
+        # end
+        #
+        # # Load in all views
+        # component_names.uniq.each do |component_name|
+        #   code = Volt::ComponentCode.new(component_name, self).code
+        #   eval(code)
+        # end
       end
     end
 
