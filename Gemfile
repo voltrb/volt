cc34d42ec1ecfa4861daa9e08e24daf7d8a16d6a
Ryan Stout <ryanstout@gmail.com>
2014-11-04 15:24:00 -0700

    WIP

--- Gemfile
+++ Gemfile
@@ -44,7 +44,6 @@ group :development do
   gem 'rubocop', require: false
   # gem 'guard', '2.0.1' # bug in current guard
   # gem 'guard-rspec'
-  # gem 'opal-rspec', '0.3.0.beta2'#, git: 'https://github.com/adambeynon/opal-rspec.git'
   # gem 'yard', require: false
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,10 @@
-puts "SPEC HELPER"
 require 'volt/spec/setup'
 
-unless RUBY_PLATFORM == 'opal'
-  # Specs are run against the kitchen sink app
-  kitchen_sink_path = File.expand_path(File.join(File.dirname(__FILE__), 'apps/kitchen_sink'))
-  Volt.spec_setup(kitchen_sink_path)
-end
+# Specs are run against the kitchen sink app
+kitchen_sink_path = File.expand_path(File.join(File.dirname(__FILE__), 'apps/kitchen_sink'))
+Volt.spec_setup(kitchen_sink_path)
 
 unless RUBY_PLATFORM == 'opal'
-
   RSpec.configure do |config|
     config.run_all_when_everything_filtered = true
     config.filter_run :focus
