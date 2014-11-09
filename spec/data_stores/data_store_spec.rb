e69e7ce70226ab8a12425dfbe309be9b04415a7c
Frank Falkenberg <faltibrain@gmail.com>
2014-11-09 12:38:27 +0100

    checking the error message

--- spec/data_stores/data_store_spec.rb
+++ spec/data_stores/data_store_spec.rb
@@ -23,7 +23,7 @@ describe Volt::DataStore do
     expect(Volt).to receive(:config).at_least(:once) { config }
     expect(config).to receive(:db_driver).at_least(:once) { 'unknown_driver' }
 
-    expect { Volt::DataStore.fetch }.to raise_error
+    expect { Volt::DataStore.fetch }.to raise_error("Database specified in Volt.config.db_driver is not supported: unknown_driver")
 
   end
 end
