fd1ab718466bb1c9da882bcd1498f7e1b9d047d0
Ryan Stout <ryanstout@gmail.com>
2014-11-09 15:31:39 -0700

    Add user in tests

--- app/volt/models/user.rb
+++ app/volt/models/user.rb
@@ -2,30 +2,32 @@ if RUBY_PLATFORM != 'opal'
   require 'bcrypt'
 end
 
-class User < Volt::Model
-  # returns true if the user configured using the username
-  def self.login_field
-    if Volt.config.public.try(:auth).try(:use_username)
-      :username
-    else
-      :email
+module Volt
+  class User < Model
+    # returns true if the user configured using the username
+    def self.login_field
+      if Volt.config.public.try(:auth).try(:use_username)
+        :username
+      else
+        :email
+      end
     end
-  end
 
-  validate login_field, unique: true, length: 8
+    validate login_field, unique: true, length: 8
 
-  if RUBY_PLATFORM == 'opal'
-    # Don't validate on the server
-    validate :password, length: 8
-  end
-
-  def password=(val)
-    if Volt.server?
-      # on the server, we bcrypt the password and store the result
-      self._hashed_password = BCrypt::Password.create(val)
-    else
-      self._password = val
+    if RUBY_PLATFORM == 'opal'
+      # Don't validate on the server
+      validate :password, length: 8
     end
-  end
 
+    def password=(val)
+      if Volt.server?
+        # on the server, we bcrypt the password and store the result
+        self._hashed_password = BCrypt::Password.create(val)
+      else
+        self._password = val
+      end
+    end
+
+  end
 end
---
+++ spec/apps/kitchen_sink/app/main/models/user.rb
@@ -0,0 +1,2 @@
+class User < Volt::User
+end
