73e795031b368d0d49442187a813c4c975c05396
Ryan Stout <ryanstout@gmail.com>
2014-11-20 20:49:25 -0700

    improve error message for missing component gems

--- lib/volt/server/rack/asset_files.rb
+++ lib/volt/server/rack/asset_files.rb
@@ -22,7 +22,7 @@ module Volt
       if File.exist?(dependencies_file)
         # Run the dependencies file in this asset files context
         code = File.read(dependencies_file)
-        instance_eval(code)
+        instance_eval(code, dependencies_file, 0)
       end
     end
 
@@ -32,7 +32,13 @@ module Volt
         @included_components[name] = true
 
         # Get the path to the component
-        @component_paths.component_paths(name).each do |path|
+        component_path = @component_paths.component_paths(name)
+
+        unless component_path
+          raise "Unable to find component '#{name}', make sure the gem is included in your Gemfile"
+        end
+
+        component_path.each do |path|
 
           # Load the dependencies
           load_dependencies(path)
