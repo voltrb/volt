bb4e52583ceba93f11de91575488635d17d2669a
Ryan Stout <ryanstout@gmail.com>
2014-11-23 13:50:33 -0700

    WIP

--- lib/volt/models/validators/user_validation.rb
+++ lib/volt/models/validators/user_validation.rb
@@ -1,23 +1,26 @@
-# module Volt
-#   # UserValidator gives you two
-#
-#
-#
-#   permissions do |user_id|
-#     if user_id == Volt.user_id
-#       allow_write :field1, :field2
-#       deny_write :field3
-#     end
-#   end
-#
-#
-#   class UserValidator
-#     def self.validate(model, old_model, field_name, args)
-#       errors = {}
-#
-#       message = (args.is_a?(Hash) && args[:message]) || 'can not be modified by you'
-#
-#
-#     end
-#   end
-# end
+module Volt
+  # UserValidator gives you two
+
+
+
+  own_by_user
+
+  permissions do |user_id|
+    if owner?
+      allow_write :field1, :field2
+    else
+      deny_write :field3
+    end
+  end
+
+
+  class UserValidator
+    def self.validate(model, old_model, field_name, args)
+      errors = {}
+
+      message = (args.is_a?(Hash) && args[:message]) || 'can not be modified by you'
+
+
+    end
+  end
+end
