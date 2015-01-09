ec20771952836bc73b7b3228578fe231e6ffbe71
Luke Barbuto <luke.barbuto@gmail.com>
2015-01-09 14:54:34 -0800

    Refactor validation specs

--- spec/models/validations_spec.rb
+++ spec/models/validations_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
-class TestModel < Volt::Model
-  validate :count, numericality: { min: 5, max: 10 }
-  validate :description, length: { message: 'needs to be longer', length: 50 }
-  validate :email, email: true
-  validate :name, length: 4
-  validate :phone_number, phone_number: true
-  validate :username, presence: true
-end
-
 describe Volt::Model do
-  it 'should validate the name' do
-    expect(TestModel.new.errors).to eq(
+  let(:model) { test_model_class.new }
+
+  let(:test_model_class) do
+    Class.new(Volt::Model) do
+      validate :count, numericality: { min: 5, max: 10 }
+      validate :description, length: { message: 'needs to be longer',
+                                       length: 50 }
+      validate :email, email: true
+      validate :name, length: 4
+      validate :phone_number, phone_number: true
+      validate :username, presence: true
+    end
+  end
+
+  it 'should return errors for all failed validations' do
+    expect(model.errors).to eq(
       count: ['must be a number'],
       description: ['needs to be longer'],
       email: ['must be an email address'],
@@ -21,23 +26,7 @@ describe Volt::Model do
     )
   end
 
-  it 'should show marked validations once they are marked' do
-    model = TestModel.new
-
-    expect(model.marked_errors).to eq({})
-
-    model.mark_field!(:name)
-
-    expect(model.marked_errors).to eq(
-      name: ['must be at least 4 characters']
-    )
-  end
-
   it 'should show all fields in marked errors once saved' do
-    model = TestModel.new
-
-    expect(model.marked_errors).to eq({})
-
     model.save!
 
     expect(model.marked_errors.keys).to eq(
@@ -45,73 +34,43 @@ describe Volt::Model do
     )
   end
 
-  describe 'length' do
-    it 'should allow custom errors on length' do
-      model = TestModel.new
-
-      expect(model.marked_errors).to eq({})
-
-      model.mark_field!(:description)
-
-      expect(model.marked_errors).to eq(
-        description: ['needs to be longer']
-      )
+  describe 'builtin validations' do
+    shared_examples_for 'a built in validation' do |field, message|
+      specify do
+        expect { model.mark_field! field }
+          .to change { model.marked_errors }
+          .from({}).to({ field => [message ] })
+      end
     end
-  end
 
-  describe 'presence' do
-    it 'should validate presence' do
-      model = TestModel.new
-
-      expect(model.marked_errors).to eq({})
-
-      model.mark_field!(:username)
-
-      expect(model.marked_errors).to eq(
-        username: ['must be specified']
-      )
+    describe 'numericality' do
+      message = 'must be a number'
+      it_should_behave_like 'a built in validation', :count, message
     end
-  end
 
-  describe 'numericality' do
-    it 'should validate numericality' do
-      model = TestModel.new
-
-      expect(model.marked_errors).to eq({})
-
-      model.mark_field!(:count)
-
-      expect(model.marked_errors).to eq(
-        count: ['must be a number']
-      )
+    describe 'length' do
+      message = 'needs to be longer'
+      it_should_behave_like 'a built in validation', :description, message
     end
-  end
 
-  describe 'email' do
-    it 'should validate email' do
-      model = TestModel.new
-
-      expect(model.marked_errors).to eq({})
-
-      model.mark_field!(:email)
-
-      expect(model.marked_errors).to eq(
-        email: ['must be an email address']
-      )
+    describe 'email' do
+      message = 'must be an email address'
+      it_should_behave_like 'a built in validation', :email, message
     end
-  end
 
-  describe 'phone_number' do
-    it 'should validate phone number' do
-      model = TestModel.new
+    describe 'name' do
+      message = 'must be at least 4 characters'
+      it_should_behave_like 'a built in validation', :name, message
+    end
 
-      expect(model.marked_errors).to eq({})
+    describe 'phone_number' do
+      message = 'must be a phone number with area or country code'
+      it_should_behave_like 'a built in validation', :phone_number, message
+    end
 
-      model.mark_field!(:phone_number)
-
-      expect(model.marked_errors).to eq(
-        phone_number: ['must be a phone number with area or country code']
-      )
+    describe 'presence' do
+      message = 'must be specified'
+      it_should_behave_like 'a built in validation', :username, message
     end
   end
 end
