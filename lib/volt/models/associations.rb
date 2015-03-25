ac59b8dd2dbbb85243ff4de4f0a2ef9c9e2f53df
Ryan Stout <ryanstout@gmail.com>
2015-03-25 11:59:41 -0600

    add mongo service for travis

--- lib/volt/models/associations.rb
+++ lib/volt/models/associations.rb
@@ -3,14 +3,31 @@ module Volt
     module ClassMethods
       def belongs_to(method_name, key_name=nil)
         define_method(method_name) do
-          # Get the root node
-          root = persistor.try(:root_model) || $page.page
+          persistor = self.persistor || (respond_to(:save_to) && save_to.persistor)
+          # Check if we are on the store collection
+          if persistor.is_a?(Volt::Persistors::ModelStore)
+            # Get the root node
+            root = persistor.try(:root_model) || $page.page
 
-          # Lookup the associated model id
-          lookup_key = send(:"_#{key_name || method_name}_id")
+            # Lookup the associated model id
+            lookup_key = send(:"_#{key_name || method_name}_id")
 
-          # Return a promise for the belongs_to
-          root.send(:"_#{method_name.pluralize}").where(:_id => lookup_key).fetch_first
+            # Return a promise for the belongs_to
+            root.send(:"_#{method_name.pluralize}").where(:_id => lookup_key).fetch_first
+          else
+            fail "belongs_to currently only works on the store collection (support for other collections coming soon)"
+          end
+        end
+      end
+
+      def has_many(method_name, key_name=nil)
+        persistor = self.persistor || (respond_to(:save_to) && save_to.persistor)
+
+        # Check if we are on the store collection
+        if persistor.is_a?(Volt::Persistors::ModelStore)
+
+        else
+          fail "has_many currently only works on the store collection (support for other collections coming soon)"
         end
       end
     end
