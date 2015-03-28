455af7f07126d6554ef96b44448d6106a82a1e11
Julian Fahrer <julian@fahrer.net>
2015-03-28 17:46:38 +0100

    Requiring http_controllers

--- lib/volt/server.rb
+++ lib/volt/server.rb
@@ -26,9 +26,13 @@ if RUBY_PLATFORM != 'java'
 end
 require 'volt/server/rack/component_paths'
 require 'volt/server/rack/index_files'
+require 'volt/server/rack/http_resource'
 require 'volt/server/rack/opal_files'
 require 'volt/server/rack/quiet_common_logger'
 require 'volt/page/page'
+
+require 'volt/server/rack/http_request'
+require 'volt/controllers/http_controller'
 
 module Rack
   # TODO: For some reason in Rack (or maybe thin), 304 headers close
@@ -53,6 +57,7 @@ end
 
 module Volt
   class Server
+
     def initialize(root_path = nil)
       root_path ||= Dir.pwd
       Volt.root = root_path
@@ -62,6 +67,8 @@ module Volt
       # Boot the volt app
       @component_paths = Volt.boot(root_path)
 
+      setup_router
+      require_http_controllers
       setup_change_listener
 
       display_welcome
@@ -71,16 +78,39 @@ module Volt
       puts File.read(File.join(File.dirname(__FILE__), 'server/banner.txt'))
     end
 
+    def setup_router
+      # Find the route file
+      home_path  = @component_paths.component_paths('main').first
+      routes = File.read("#{home_path}/config/routes.rb")
+      @router = Routes.new.define do
+        eval(routes)
+      end
+    end
+
+    def require_http_controllers
+      @component_paths.app_folders do |app_folder|
+        # Sort so we get consistent load order across platforms
+        Dir["#{app_folder}/*/{http_controllers}/*.rb"].each do |ruby_file|
+          path = ruby_file.gsub(/^#{app_folder}\//, '')[0..-4]
+          require(path)
+        end
+      end
+    end
+
     def setup_change_listener
       # Setup the listeners for file changes
       listener = Listen.to("#{@app_path}/") do |modified, added, removed|
         puts 'file changed, sending reload'
+        setup_router
+        require_http_controllers
         SocketConnectionHandler.send_message_all(nil, 'reload')
       end
       listener.start
     end
 
     def app
+      puts "Im getting called"
+      
       @app = Rack::Builder.new
 
       # Should only be used in production
@@ -110,6 +140,8 @@ module Volt
       # which JS/CSS files to serve.
       @app.use IndexFiles, @component_paths, opal_files
 
+      @app.use HttpResource, @router
+
       component_paths.require_in_components
 
       # Handle socks js connection
@@ -122,16 +154,16 @@ module Volt
       end
 
       @app.use Rack::Static,
-               urls: ['/'],
-               root: 'config/base',
-               index: '',
-               header_rules: [
-                 [:all, { 'Cache-Control' => 'public, max-age=86400' }]
-               ]
+      urls: ['/'],
+      root: 'config/base',
+      index: '',
+      header_rules: [
+       [:all, { 'Cache-Control' => 'public, max-age=86400' }]
+     ]
 
-      @app.run lambda { |env| [404, { 'Content-Type' => 'text/html; charset=utf-8' }, ['404 - page not found']] }
+     @app.run lambda { |env| [404, { 'Content-Type' => 'text/html; charset=utf-8' }, ['404 - page not found']] }
 
-      @app
-    end
-  end
+     @app
+   end
+ end
 end
