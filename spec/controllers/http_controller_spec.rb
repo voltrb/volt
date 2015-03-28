7ca87db5815c9b6ab1c3e4afcf343b46a3df07e5
Julian Fahrer <julian@fahrer.net>
2015-03-28 10:32:27 +0100

    Fixed a thing with opal and the specs

--- spec/controllers/http_controller_spec.rb
+++ spec/controllers/http_controller_spec.rb
@@ -1,65 +1,69 @@
-require 'volt/controllers/http_controller'
-require 'volt/server/rack/http_request'
+if RUBY_PLATFORM != 'opal'
+  require 'volt/controllers/http_controller'
+  require 'volt/server/rack/http_request'
 
-describe Volt::HttpController do
+  describe Volt::HttpController do
 
-	class TestHttpController < Volt::HttpController
+    class TestHttpController < Volt::HttpController
 
-		attr_reader :action_called
+      attr_reader :action_called
 
-		def just_call_an_action
-			@action_called = true
-		end
+      def just_call_an_action
+        @action_called = true
+      end
 
-		def ok_head_action
-			head :ok, location: "http://example.com"
-		end
+      def ok_head_action
+        head :ok, location: "http://example.com"
+      end
 
-		def created_head_action
-			head :created
-		end
+      def created_head_action
+        head :created
+      end
 
-		def redirect_action
-			redirect_to "http://path.to/example"
-		end
+      def redirect_action
+        redirect_to "http://path.to/example"
+      end
 
-		def render_action
-			render "just plain text"
-		end
-	end
+      def render_action
+        render "just plain text"
+      end
+    end
 
-	let(:request) {
-		Volt::HttpRequest.new(Rack::MockRequest.env_for("http://example.com/test.html", "CONTENT_TYPE" => "text/plain;charset=utf-8"))
-	}
+    let(:request) {
+      Volt::HttpRequest.new(Rack::MockRequest.env_for("http://example.com/test.html", "CONTENT_TYPE" => "text/plain;charset=utf-8"))
+    }
 
-  it "should perform the correct action" do
-  	controller = TestHttpController.new(request)
-	 	expect(controller.action_called).not_to be(true)
-  	controller.perform(:just_call_an_action)
-  	expect(controller.action_called).to be(true)
+    it "should perform the correct action" do
+      controller = TestHttpController.new(request)
+      expect(controller.action_called).not_to be(true)
+      controller.perform(:just_call_an_action)
+      expect(controller.action_called).to be(true)
+    end
+
+    it "should redirect" do
+      controller = TestHttpController.new(request)
+      expect(controller.action_called).not_to be(true)
+      response = controller.perform(:redirect_action)
+      expect(response.redirect?).to be(true)
+      expect(response.location).to eq("http://path.to/example")
+    end
+
+    it "should render plain text" do
+      controller = TestHttpController.new(request)
+      expect(controller.action_called).not_to be(true)
+      response = controller.perform(:render_action)
+      expect(response.body).to eq(["just plain text"])
+      expect(response['Content-Type']).to eq("text/plain")
+    end
+
+    it "should set the correct content type based on the format"
+
+    it "should use a renderer"
+
+    it "should camalize and dasherize the headers"
+
+    it "removes the content for status codes 204, ..."
+
+
   end
-
-  it "should redirect" do
-  	controller = TestHttpController.new(request)
-	 	expect(controller.action_called).not_to be(true)
-  	response = controller.perform(:redirect_action)
-  	expect(response.redirect?).to be(true)
-  	expect(response.location).to eq("http://path.to/example")
-  end
-
-  it "should render plain text" do
-  	controller = TestHttpController.new(request)
-	 	expect(controller.action_called).not_to be(true)
-  	response = controller.perform(:render_action)
-  	expect(response.body).to eq(["just plain text"])
-  	expect(response['Content-Type']).to eq("text/plain")
-  end
-
-  it "should set the correct content type based on the format"
-
-  it "should use a renderer"
-
-  it "should camalize and dasherize the headers"
-
-  it "removes the content for status codes 204, ..."
 end
--- spec/server/rack/http_resource_spec.rb
+++ spec/server/rack/http_resource_spec.rb
@@ -1,37 +1,38 @@
-require 'volt/server/rack/http_resource'
-require 'volt/server/rack/http_request'
-require 'volt/controllers/http_controller'
-require 'rack'
+if RUBY_PLATFORM != 'opal'
+  require 'volt/server/rack/http_resource'
+  require 'volt/controllers/http_controller'
+  require 'volt/server/rack/http_request'
 
-describe Volt::HttpResource do
+  describe Volt::HttpResource do
 
-  class SimpleController < Volt::HttpController
-    attr_reader :action_called
+    class SimpleController < Volt::HttpController
+      attr_reader :action_called
 
-    def index
-      @action_called = true
-      render "just some text"
+      def index
+        @action_called = true
+        render "just some text"
+      end
     end
-  end
 
-  let(:app) { lambda {|env| [404, env, "app"] } }
-  let(:http_resource) { Volt::HttpResource.new(app) }
-  let(:env) { Rack::MockRequest.env_for("http://example.com/http_controller_test/simple_controller") }
-  let(:request) { Volt::HttpRequest.new(env) }
+    let(:app) { lambda {|env| [404, env, "app"] } }
+    let(:http_resource) { Volt::HttpResource.new(app) }
+    let(:env) { Rack::MockRequest.env_for("http://example.com/http_controller_test/simple_controller") }
+    let(:request) { Volt::HttpRequest.new(env) }
 
-  let! :test_controller do
-    SimpleController.new(request)
-  end
+    let! :test_controller do
+      SimpleController.new(request)
+    end
 
-  before(:each) do
-    allow(SimpleController).to receive(:new).and_return(test_controller)
-  end
+    before(:each) do
+      allow(SimpleController).to receive(:new).and_return(test_controller)
+    end
 
-  it "should initialize the correct controller" do
-    expect(SimpleController).to receive(:new).and_return(test_controller)
-    response = http_resource.call(env)
-    expect(response.body).to eq(["just some text"])
-    expect(response.status).to eq(200)
-    expect(test_controller.action_called).to eq(true)
+    it "should initialize the correct controller" do
+      expect(SimpleController).to receive(:new).and_return(test_controller)
+      response = http_resource.call(env)
+      expect(response.body).to eq(["just some text"])
+      expect(response.status).to eq(200)
+      expect(test_controller.action_called).to eq(true)
+    end
   end
 end
