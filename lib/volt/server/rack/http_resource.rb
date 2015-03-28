637ec5c62a4a9cb89650176d0a301f610ef41166
Julian Fahrer <julian@fahrer.net>
2015-03-28 16:29:34 +0100

    Finalized middleware for http_controller

--- lib/volt/server/rack/http_resource.rb
+++ lib/volt/server/rack/http_resource.rb
@@ -1,26 +1,20 @@
+require 'rack'
 require 'volt'
 require 'volt/router/routes'
 require 'volt/server/rack/http_request'
-require 'rack'
 
 module Volt
   # Rack middleware for HttpController
   class HttpResource
-    def initialize(app)
+    def initialize(app, router)
       @app = app
-      #@@router ||= Routes.new.define do
-        # Find the route file
-        #home_path  = component_paths.component_paths('main').first
-        #route_file = File.read("#{home_path}/config/routes.rb")
-        #eval(route_file)
-      #end
+      @router = router
     end
 
     def call(env)
-      path = env['PATH_INFO']
-      if controller_name = routes_match?(path)
-        controller = Object.const_get(controller_name.camelize.to_sym).new(HttpRequest.new(env))
-        controller.perform(:index)
+      request = HttpRequest.new(env)
+      if params = routes_match?(request)
+        dispatch_to_controller(params, request)
       else
         @app.call env
       end
@@ -28,9 +22,15 @@ module Volt
 
     private
 
-    def routes_match?(path)
-      matched = path.match(/^\/http_controller_test\/(.+)/)
-      matched.present? ? matched[1] : false
+    def routes_match?(request)
+      @router.url_to_params(request.method, request.path)
+    end
+
+    def dispatch_to_controller(params, request)
+      controller_name = params[:_controller] + "_controller"
+      action = params[:_action]
+      controller = Object.const_get(controller_name.camelize.to_sym).new(params, request)
+      controller.perform(action)
     end
   end
 end
--- spec/server/rack/http_resource_spec.rb
+++ spec/server/rack/http_resource_spec.rb
@@ -2,37 +2,61 @@ if RUBY_PLATFORM != 'opal'
   require 'volt/server/rack/http_resource'
   require 'volt/controllers/http_controller'
   require 'volt/server/rack/http_request'
+  require 'volt/router/routes'
 
   describe Volt::HttpResource do
+
+    def routes(&block)
+      @routes = Volt::Routes.new
+      @routes.define(&block)
+    end
 
     class SimpleController < Volt::HttpController
       attr_reader :action_called
 
       def index
         @action_called = true
-        render "just some text"
+        render plain: "just some text"
       end
+
+      def show
+        render plain: "show with id #{params[:stuff_id]} and #{params[:test]} called"
+      end
+
     end
 
     let(:app) { lambda {|env| [404, env, "app"] } }
-    let(:http_resource) { Volt::HttpResource.new(app) }
-    let(:env) { Rack::MockRequest.env_for("http://example.com/http_controller_test/simple_controller") }
-    let(:request) { Volt::HttpRequest.new(env) }
-
-    let! :test_controller do
-      SimpleController.new(request)
-    end
 
     before(:each) do
-      allow(SimpleController).to receive(:new).and_return(test_controller)
+      routes do
+        get '/stuff', _controller: 'simple', _action: 'index'
+        get '/stuff/{{ stuff_id }}', _controller: 'simple', _action: 'show'
+      end      
     end
 
-    it "should initialize the correct controller" do
-      expect(SimpleController).to receive(:new).and_return(test_controller)
+
+    it "should initialize the correct controller and call the correct action" do
+      http_resource = Volt::HttpResource.new(app, @routes)
+      env = Rack::MockRequest.env_for("http://example.com/stuff")
+      request = Volt::HttpRequest.new(env)
+      controller = SimpleController.new({}, request)
+      expect(SimpleController).to receive(:new).and_return(controller)
+
       response = http_resource.call(env)
-      expect(response.body).to eq(["just some text"])
       expect(response.status).to eq(200)
-      expect(test_controller.action_called).to eq(true)
+      expect(response.body).to eq(["just some text"])
+      expect(controller.action_called).to eq(true)
     end
+
+    it "should parse the correct params to the controller" do
+      http_resource = Volt::HttpResource.new(app, @routes)
+      env = Rack::MockRequest.env_for("http://example.com/stuff/99?test=another_param")
+      request = Volt::HttpRequest.new(env)
+
+      response = http_resource.call(env)
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(["show with id 99 and another_param called"])
+    end
+
   end
 end
