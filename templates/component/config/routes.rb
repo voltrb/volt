8cd482846768668704b5ad8cd99fbc0967ee9c4b
Julian Fahrer <julian@fahrer.net>
2015-04-09 13:39:02 +0200

    Removed routes for the component template

--- templates/component/config/routes.rb
+++ templates/component/config/routes.rb
@@ -1,6 +1,1 @@
 # See https://github.com/voltrb/volt#routes for more info on routes
-
-client '/about', action: 'about'
-
-# The main route, this should be last.  It will match any params not previously matched.
-client '/', {}
