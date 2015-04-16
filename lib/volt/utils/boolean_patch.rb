cc760e83e17299d7c54885a25e4f8aa5f0b98d14
Ryan Stout <ryanstout@gmail.com>
2015-04-16 00:26:34 -0600

    one more gem pre-release

--- lib/volt/utils/boolean_patch.rb
+++ lib/volt/utils/boolean_patch.rb
@@ -1,4 +1,4 @@
-# A patch to fix https://github.com/opal/opal/issues/801 until it is fixed
+# A patch to fix https://github.com/opal/opal/issues/801 until it is fixed in (0.8)
 if RUBY_PLATFORM == 'opal'
   class Boolean
     def hash
