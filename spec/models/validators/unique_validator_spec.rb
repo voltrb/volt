fac4bc01d6a20d3c92f66e998021434b052835ea
Ryan Stout <ryanstout@gmail.com>
2015-04-26 23:25:43 -0600

    Skip opal for unique validator spec.

--- spec/models/validators/unique_validator_spec.rb
+++ spec/models/validators/unique_validator_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
-class Fridge < Volt::Model
-  validate :name, unique: true
-end
-
-
-describe 'unique spec' do
-  it 'should reject save if there are records with existing attributes already' do
-    store._fridges << { name: 'swift' }
-    fridge = store._fridges.buffer name: 'swift'
-    fridge.save!.then do
-      expect(false).to be_true
-    end.fail do
-      expect(true).to be_true
-    end
+unless RUBY_PLATFORM == 'opal'
+  class Fridge < Volt::Model
+    validate :name, unique: true
   end
 
-  it 'should not increase count of the total records in the store' do
-    store._fridges << { name: 'swift' }
-    store._fridges << { name: 'swift' }
-    expect(store._fridges.count).to eq(1)
+
+  describe 'unique spec' do
+    it 'should reject save if there are records with existing attributes already' do
+      store._fridges << { name: 'swift' }
+      fridge = store._fridges.buffer name: 'swift'
+      fridge.save!.then do
+        expect(false).to be_true
+      end.fail do
+        expect(true).to be_true
+      end
+    end
+
+    it 'should not increase count of the total records in the store' do
+      store._fridges << { name: 'swift' }
+      store._fridges << { name: 'swift' }
+      expect(store._fridges.count).to eq(1)
+    end
   end
 end
