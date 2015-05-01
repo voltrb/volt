a5384dc403bddfbe07b334bd8fee52645928e516
Ryan Stout <ryanstout@gmail.com>
2015-05-01 11:34:22 -0600

    Cleanup and take options for rendered from path

--- lib/volt/page/path_string_renderer.rb
+++ lib/volt/page/path_string_renderer.rb
@@ -8,12 +8,15 @@ require 'volt/page/string_template_renderer'
 module Volt
   class PathStringRenderer
     attr_reader :html
-    def initialize(path, attrs=nil, page=nil)
+    def initialize(path, attrs=nil, page=nil, render_from_path=nil)
       # use the global page if one is not passed in
       page ||= $page
 
+      # where to do the path lookup from
+      render_from_path ||= "main/main/main/body"
+
       # Make path into a full path
-      @view_lookup = Volt::ViewLookupForPath.new(page, "main/main/main/body")
+      @view_lookup = Volt::ViewLookupForPath.new(page, render_from_path)
       full_path, controller_path = @view_lookup.path_for_template(path, nil)
 
       controller_class, action = ControllerHandler.get_controller_and_action(controller_path)
