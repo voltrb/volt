bfeb864e1f9c1a0c853e9aee6a6922e781865284
Ryan Stout <ryanstout@gmail.com>
2015-05-02 16:45:51 -0600

    Add support for namespaced Tasks

--- lib/volt/server/component_templates.rb
+++ lib/volt/server/component_templates.rb
@@ -100,8 +100,21 @@ module Volt
 
     def generate_tasks_code
       Task.known_handlers.map do |handler|
-        "class #{handler.name} < Volt::Task; end"
-      end.join "\n"
+        # Split into modules and class
+        klass_parts = handler.name.split('::')
+
+        # Start with the inner class
+        parts = ["class #{klass_parts.pop} < Volt::Task; end"]
+
+        # Work backwards on the modules
+        klass_parts.reverse.each do |kpart|
+          parts.unshift("module #{kpart}")
+          parts.push("end")
+        end
+
+        # Combine the parts
+        parts.join("\n")
+      end.join "\n" # combine all into one string
     end
   end
 end
