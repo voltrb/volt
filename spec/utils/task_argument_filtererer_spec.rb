ab42c8754cfeda3c3df39a753c118748c3000e8b
Jason Goldberger <jlgoldb2@asu.edu>
2015-05-07 09:33:50 -0700

    added test for TaskArgumentFilterer.filter

--- spec/utils/task_argument_filtererer_spec.rb
+++ spec/utils/task_argument_filtererer_spec.rb
@@ -13,5 +13,13 @@ if RUBY_PLATFORM != 'opal'
 
       expect(filtered_args).to eq([:login, {:login=>"jim@jim.com", :password=>"[FILTERED]"}])
     end
+
+    it 'should create and run a new TaskArgumentFilterer when its filter method is called' do
+      filtered_args = TaskArgumentFilterer.new([{login: 'jim@jim.com', password: 'some password'}]).run
+      expect(filtered_args).to eq([{:login=>"jim@jim.com", :password=>"[FILTERED]"}])
+      another_filtered_args = TaskArgumentFilterer.filter([{login: 'jam@jam.com', password: 'some password'}])
+      expect(another_filtered_args).to eq([{:login=>"jam@jam.com", :password=>"[FILTERED]"}])
+    end
+
   end
 end
