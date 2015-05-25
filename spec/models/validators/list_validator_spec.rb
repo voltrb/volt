84fdfb5f9889191cbda72f741ce7aa4d3b1dfea7
Matt Hale <matt.hale.0@gmail.com>
2015-05-25 13:08:46 -0700

    Add a list validator to allow validating that a value is included in an array.

---
+++ spec/models/validators/list_validator_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe Volt::ListValidator, focus: true do
+  subject { Volt::ListValidator.validate(*params) }
+  let(:params) { [model, field_name, options] }
+
+  let(:model) { Volt::Model.new name: name }
+  let(:field_name) { :name }
+  let(:name) { 'John' }
+
+  describe '.validate' do
+    describe 'when options is an array' do
+      let(:options) { ['John', 'Susie', 'Mary'] }
+
+      describe 'when name is "John"' do
+        let(:name) { 'John' }
+        it { expect(subject).to eq({}) }
+      end
+
+      describe 'when name is "Bill"' do
+        let(:name) { 'Bill' }
+        it do
+          expect(subject).to eq(name: ['must be one of John, Susie, Mary'])
+        end
+      end
+    end
+
+    describe 'when options is a Hash' do
+      let(:options) do
+        { list: ['John', 'Susie', 'Mary'], message: 'Choose one from the list.' }
+      end
+
+      describe 'when name is "John"' do
+        let(:name) { 'John' }
+        it { expect(subject).to eq({}) }
+      end
+
+      describe 'when name is "Bill"' do
+        let(:name) { 'Bill' }
+        it do
+          expect(subject).to eq(name: ['Choose one from the list.'])
+        end
+      end
+
+    end
+
+    describe 'when options not a Fixnum or a Hash' do
+      let(:options) { 'string' }
+
+      it 'raises an exception' do
+        expect { subject }.to raise_error(
+          RuntimeError,
+          'The arguments to list validator must be an array or a hash'
+        )
+      end
+    end
+  end
+end
