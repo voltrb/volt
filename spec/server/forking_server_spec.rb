d4dc3269f35f207e83c86f8f7c2db14e4d857ab5
Ryan Stout <ryanstout@gmail.com>
2015-08-02 15:22:52 -0600

    fix spec

--- spec/server/forking_server_spec.rb
+++ spec/server/forking_server_spec.rb
@@ -4,13 +4,14 @@ unless RUBY_PLATFORM == 'opal'
 
   describe Volt::ForkingServer do
     it 'should set polling an an option when using POLL_FS env' do
+      ENV['POLL_FS'] = 'true'
       forking_server = Volt::ForkingServer.allocate
 
       # Lots of stubs, since we're working with the FS
       listener = double('listener')
       expect(listener).to receive(:start)
       expect(listener).to receive(:stop)
-      expect(Listen).to receive(:to).and_return(listener)
+      expect(Listen).to receive(:to).with('/app/', {force_polling: true}).and_return(listener)
       expect(forking_server).to receive(:sync_mod_time)
 
       server = double('server')
@@ -18,6 +19,7 @@ unless RUBY_PLATFORM == 'opal'
       forking_server.instance_variable_set(:@server, server)
 
       forking_server.start_change_listener
+      ENV.delete('POLL_FS')
 
       forking_server.stop_change_listener
     end
