3733584454c8ca06e7956800abe8ff8ad144dd4b
Ryan Stout <ryanstout@gmail.com>
2015-08-02 15:20:24 -0600

    Add POLL_FS option to forking server.

---
+++ spec/server/forking_server_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+unless RUBY_PLATFORM == 'opal'
+  require 'volt/server'
+
+  describe Volt::ForkingServer do
+    it 'should set polling an an option when using POLL_FS env' do
+      forking_server = Volt::ForkingServer.allocate
+
+      # Lots of stubs, since we're working with the FS
+      listener = double('listener')
+      expect(listener).to receive(:start)
+      expect(listener).to receive(:stop)
+      expect(Listen).to receive(:to).and_return(listener)
+      expect(forking_server).to receive(:sync_mod_time)
+
+      server = double('server')
+      expect(server).to receive(:app_path).and_return('/app')
+      forking_server.instance_variable_set(:@server, server)
+
+      forking_server.start_change_listener
+
+      forking_server.stop_change_listener
+    end
+  end
+end
