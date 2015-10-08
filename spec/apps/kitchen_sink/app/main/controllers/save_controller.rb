4faa224985a70dc07828b5c8d19e6942346eebdb
Ryan Stout <ryanstout@gmail.com>
2015-10-08 16:17:36 -0600

    cleanup logging

--- spec/apps/kitchen_sink/app/main/controllers/save_controller.rb
+++ spec/apps/kitchen_sink/app/main/controllers/save_controller.rb
@@ -2,7 +2,6 @@ module Main
   class SaveController < Volt::ModelController
     def add_post
       store.posts.create(page._new_post.to_h).fail do |err|
-        puts err.inspect
         flash._notices << err.inspect
       end
     end
