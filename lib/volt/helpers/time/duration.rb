dc444e1f68e2077814f0ecd139f45356de98042c
David Boardman <david.boardman@lamanva.com>
2015-10-24 15:41:23 -0700

    Fix spacing bug with Duration#inspect

--- lib/volt/helpers/time/duration.rb
+++ lib/volt/helpers/time/duration.rb
@@ -71,7 +71,7 @@ module Volt
           when 2
             "#{array[0]} and #{array[1]}"
           else
-            "#{array[0...-1].join(',')} and #{array[-1]}"
+            "#{array[0...-1].join(', ')} and #{array[-1]}"
         end
       end
 
