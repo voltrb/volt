befc9bae120de5ac92358c1d40ec8d35db6d93ff
Antonius Ostermann <ostermann.antonius@gmail.com>
2016-01-05 20:14:16 +0100

    finishes infrastructure of publishing and subscribing

--- app/volt/tasks/message_bus_tasks.rb
+++ app/volt/tasks/message_bus_tasks.rb
@@ -1,6 +1,53 @@
 class MessageBusTasks < Volt::Task
+  require 'securerandom'
+
+  # Publishes a message in the message bus
   def publish(channel, message)
     Volt.current_app.message_bus.publish(channel, message)
     return true
+  end
+
+  # Subscribe to specific events. Returns a listener_id, useful for unsubscribing
+  def subscribe(*events)
+    listener_id = generate_listener_id
+    @@subscriptions ||= {}
+    @@subscriptions[listener_id] = []
+
+    events.each do |event|
+      @@subscriptions[listener_id] << Volt.current_app.message_bus.on(event) do |msg|
+        inform_subscriber(event, msg)
+      end
+    end
+
+    return listener_id
+  end
+
+  # Removes a subscription, needs the listener_id (see #subscribe for more info)
+  def remove(listener_id)
+    if @@subscriptions && @@subscriptions[listener_id]
+      @@subscriptions[listener_id].each &:remove
+    end
+
+    return listener_id
+  end
+
+
+
+  # todo: react on disconnect / connect
+  # todo: authentication / authorization layer
+  # todo: authenticate publishing and subscribing
+  # todo: reauthenticate publish/subscribe on each message
+  # todo: does publishing also fire the event locally?
+
+  private
+
+  # informs subscriber about new message in channel
+  def inform_subscriber(channel, msg)
+    @channel.send_message('message_bus_event', channel, msg)
+  end
+
+  # Just returns a random listener_id
+  def generate_listener_id
+    SecureRandom.uuid
   end
 end
--- lib/volt/page/message_bus_client_proxy.rb
+++ lib/volt/page/message_bus_client_proxy.rb
@@ -1,27 +1,73 @@
 require 'volt/reactive/eventable'
+require 'volt/server/message_bus/base_message_bus'
 
 module Volt
-  class MessageBusClientProxy
+  class MessageBusClientProxy < MessageBus::BaseMessageBus
     include Eventable
+
+    # Custom listener class, proxying Eventable#Listener since
+    # we have to inform remote on the removal of a listener
+    class ListenerProxy
+      def initialize(target, remote_listener_id)
+        @target = target
+        @listener_id = remote_listener_id
+      end
+
+      # custom remove implementation: also calls task to remove listener
+      # returns promise of the task
+      def remove
+        @target.remove
+        MessageBusTasks.remove(@listener_id)
+      end
+
+      # proxy all other methods
+      def method_missing(method, *args, &block)
+        @target.send(method, *args, &block)
+      end
+    end
 
     # Use subscribe instead of on provided in Eventable
     alias_method :subscribe, :on
+    alias_method :eventable_on, :on # this is only for obtaining the original
+                                    # method behaviour although overriding it
 
     # Adds a reference to the client app from this proxy
     def initialize(volt_app)
       @volt_app = volt_app
+
+      # Called when the backend informs us about a new subscribed message bus event
+      @volt_app.channel.on('message') do |*args|
+        if args.delete_at(0) == 'message_bus_event'
+          trigger!(*args)
+        end
+      end
     end
 
-    # publish should push out to all subscribed within the volt cluster.
+    # Publishes a message into the message bus, returns a promise
     def publish(channel_name, message)
-      puts 'publishing to channel...'
-      MessageBusTasks.publish(channel_name, message).then do |result|
-        puts 'success'
-        puts result
-      end.fail do |fail|
-        puts 'error'
-        puts fail
+      MessageBusTasks.publish(channel_name, message)
+    end
+
+    # overwrites subscribe and on from Eventable to register subscription in message bus first
+    # this will return a promise resolving to ListenerProxy, so you can call ".remove" on it directly
+    def on(*events, &block)
+      # Promise to resolve on working subscription, giving you a listener to remove
+      subscription_promise = Promise.new
+
+      MessageBusTasks.subscribe(*events).then do |remote_listener_id|
+        # Register event locally, TODO: direclty pass block
+        listener = eventable_on(*events) do |*params|
+          block.call(*params)
+        end
+
+        # Resolve promise with object of ListenerProxy to enable removing of listener
+        subscription_promise.resolve(ListenerProxy.new(listener, remote_listener_id))
+      end.fail do |error|
+        # Tell promise about failure
+        subscription_promise.reject(error)
       end
+
+      subscription_promise
     end
 
     # Unnecessary on clients
